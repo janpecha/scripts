69c0c33bc6ecdbf2f97e01b7a9a87c246d9335d9
Jan Pecha <janpecha@email.cz>
2012-11-13 12:41:34 +0100

    added `run-editor.sh`
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ run-editor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+url="$1"
+
+url=${url#*file=}
+line=${url##*line=}
+file=${url%%&line*}
+file=${file//\%2F/\/}
+
+# Netbeans
+#netbeans "$file" $line
+
+# Kate
+#kate --line $line "$file"
+
+# Vim
+#vim "$file" +$line
+
+# Gedit
+gedit +$line "$file"
+
+# Komodo
+#komodo "$file#$line"
